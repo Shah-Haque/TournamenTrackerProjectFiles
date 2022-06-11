7d1c7043fc7d25f95d9ed55da7abb92e1753e228
Shah Haque <93956445+Shah-Haque@users.noreply.github.com>
2022-06-11 12:14:51 +0100

    Add files via upload

---
+++ Global.asax.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using TrackerLibrary;
+
+namespace TournamentTrackerMVCUI
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            //intialize the database and text file connections
+            TrackerLibrary.GlobalConfig.InitializeConnections(DatabaseType.sql);
+            //TrackerLibrary.GlobalConfig.InitializeConnections(DatabaseType.TextFile);
+        }
+    }
+}
